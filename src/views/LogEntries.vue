6c159ebb2003721b6cb7dfdc33164c1e5be72587
Sam Warnick <sam.t.warnick@gmail.com>
2018-10-27 13:20:34 -0600

    Add page for log entries

---
+++ src/views/LogEntries.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>log entries</h1>
+    <h2>something else</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LogEntries"
+};
+</script>
+
+<style scoped>
+</style>
