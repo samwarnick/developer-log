4388a8db91136c3cab03d4befe0950d22caab145
Sam Warnick <sam.t.warnick@gmail.com>
2018-10-27 13:25:09 -0600

    Add Apollo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="home">
     <h1 class="text-blue">Hello</h1>
+    <ApolloExample/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ApolloExample from "@/components/ApolloExample.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    ApolloExample
   }
 };
 </script>
