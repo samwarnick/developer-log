279b1ffb90dcd1bc0cb3232cfbe5846e31551d71
Sam Warnick <sam.t.warnick@gmail.com>
2018-11-03 14:54:09 -0600

    Fix some PurgeCSS stuff

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,26 @@
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
 const purgecss = require("@fullhuman/postcss-purgecss");
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g);
+  }
+}
+
 module.exports = {
   plugins: [
     tailwindcss("tailwind.js"),
     autoprefixer(),
     purgecss({
-      content: ["./src/**/*.html", "./src/**/*.vue"]
+      whitelist: ["p"],
+      content: ["./public/**/*.html", "./src/**/*.vue"],
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+          extensions: ["js", "vue", "html"]
+        }
+      ]
     })
   ]
 };
