d6ec55a557574073d0626ebfa66a51e09946247b
Sam Warnick <sam.t.warnick@gmail.com>
2018-11-10 17:47:51 -0700

    Mock moment filter

--- tests/unit/LogEntry.spec.js
+++ tests/unit/LogEntry.spec.js
@@ -1,11 +1,23 @@
-import { shallowMount } from "@vue/test-utils";
+import { createLocalVue, shallowMount } from "@vue/test-utils";
 import LogEntry from "@/components/LogEntry.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons/faTrashAlt";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faTrashAlt);
+
+const localVue = createLocalVue();
+localVue.filter("moment", () => {
+  return "Today!";
+});
+localVue.component("FontAwesomeIcon", FontAwesomeIcon);
 
 const mutate = jest.fn();
 
 describe("LogEntry.vue", () => {
   test("renders correctly", () => {
     const wrapper = shallowMount(LogEntry, {
+      localVue,
       propsData: {
         entry: {
           id: "1234",
@@ -19,6 +31,7 @@ describe("LogEntry.vue", () => {
 
   test("should call apollo when deleting", () => {
     const wrapper = shallowMount(LogEntry, {
+      localVue,
       propsData: {
         entry: {
           id: "1234",
